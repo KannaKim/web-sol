8e7bffff135c19042a3eb7f6e817ce8061f5829b
Kanna Kim <kimkanna18@gmail.com>
2022-02-03 15:37:14 -0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
-
+import './bootstrap-5.1.3-dist/css/bootstrap.min.css'
+import '../styles/globals.css'  
+import React,{useEffect} from 'react'
+import Head  from 'next/head'
+import  DefaultLayout  from '../Layouts/defaultLayout'
+import Footer from '../Layouts/Footer'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Head>  
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Spline+Sans:wght@400;500;700&display=swap" rel="stylesheet"></link>
+    </Head>
+
+      <DefaultLayout/>
+      <Component {...pageProps} />
+      <Footer/>
+    </>
+   )
 }
 
 export default MyApp
